ff3f305122a0d1e8e936d96b4b1d88b19b1b89f2
hugodessomme <hdessomme@gmail.com>
2019-04-04 19:43:36 +0200

    Add booking fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,6 +10,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Entity\Role;
+use App\Entity\Booking;
 
 class AppFixtures extends Fixture
 {
@@ -98,6 +99,30 @@ class AppFixtures extends Fixture
                 ;
 
                 $manager->persist($image);
+            }
+
+            // Gestion des réservations
+            for ($j=1; $j < mt_rand(0, 10); $j++) { 
+                $booking = new Booking();
+
+                $createdAt = $faker->dateTimeBetween('-6 months');
+                $startDate = $faker->dateTimeBetween('-3 months');
+                
+                $duration = mt_rand(3, 10);
+                $endDate = (clone $startDate)->modify("+$duration days");
+                $amount = $ad->getPrice() * $duration;
+
+                $booker = $users[mt_rand(0, count($users) -1)];
+
+                $booking->setBooker($booker)
+                        ->setAd($ad)
+                        ->setStartDate($startDate)
+                        ->setEndDate($endDate)
+                        ->setCreatedAt($createdAt)
+                        ->setAmount($amount)
+                ;
+
+                $manager->persist($booking);
             }
 
             $manager->persist($ad);
