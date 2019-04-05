f3e2d3b2a8ad13a6d68657b72dc7aeb496a90321
hugodessomme <hdessomme@gmail.com>
2019-04-05 16:06:28 +0200

    Save and display booking

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BookingRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Booking
 {
@@ -52,6 +53,32 @@ class Booking
      * @ORM\Column(type="text", nullable=true)
      */
     private $comment;
+
+    /**
+     * Callback appelé à chaque fois qu'on crée une réservation
+     * 
+     * @ORM\PrePersist
+     *
+     * @return void
+     */
+    public function prePersist()
+    {
+        if (empty($this->createdAt)) {
+            $this->createdAt = new \Datetime;
+        }
+
+        if (empty($this->amount)) {
+            // prix de l'annonce * nombre de jour
+            $this->amount = $this->ad->getPrice() * $this->getDuration();
+        }
+    }
+
+    public function getDuration()
+    {
+        $diff = $this->endDate->diff($this->startDate);
+
+        return $diff->days;
+    }
 
     public function getId(): ?int
     {
